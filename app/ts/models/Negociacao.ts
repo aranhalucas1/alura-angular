41a093a25708fc3a9fa8460bfacc68953a897029
Lucas Aranha <21315381@anhembimorumbi.edu.br>
2020-10-04 19:11:50 -0300

    Projeto base

---
+++ app/ts/models/Negociacao.ts
@@ -0,0 +1,27 @@
+class Negociacao {
+    private data;
+    private quantidade;
+    private valor;
+
+    constructor(data, quantidade, valor) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    // get data() {
+    //     return this.data;
+    // }
+
+    // get quantidade() {
+    //     return this.quantidade;
+    // }
+
+    // get valor() {
+    //     return this.valor;
+    // }
+
+    get volume() {
+        return this.quantidade * this.valor;
+    }
+}
